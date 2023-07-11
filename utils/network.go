a426f2597662c235c968808b4f0fda9c15c1b20b
Manan Prakash Singh <mananprakash3078@gmail.com>
2023-07-11 19:59:31 +0530

    added utils folder

---
+++ utils/network.go
@@ -0,0 +1,68 @@
+package utils
+
+import (
+	"bytes"
+	"io"
+	"net/http"
+)
+
+func NewNormalRequest(method string, url string, body []byte) (*http.Request, error) {
+    if method == "POST" {
+        request, err := http.NewRequest(method,url,bytes.NewBuffer(body))
+        if err != nil {
+            return nil, err
+        }
+        request.Header.Add("content-type","application/json")
+        return request, err
+    }
+    return http.NewRequest(method,url,nil)
+}
+
+func NewAuthRequest(method string, url string, body []byte) (*http.Request, error) {
+    key, err := getSessionKey() 
+
+    if err != nil {
+        return nil, err
+    }
+
+    request, err := NewNormalRequest(method,url,body)
+
+    if err != nil {
+        return nil, err
+    }
+    
+    request.AddCookie(&http.Cookie{
+        Name: "csrftoken",
+        Value: key.Csrftoken,
+    })
+    request.AddCookie(&http.Cookie{
+        Name: "LEETCODE_SESSION",
+        Value: key.LeetcodeSession,
+    })
+
+    request.Header.Set("Referer", "https://leetcode.com/")
+	request.Header.Set("User-Agent", "Mozilla/6.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/110.0.0.0 Safari/537.36")
+	request.Header.Set("X-csrftoken", key.Csrftoken)
+
+	return request, nil
+}
+
+func SendRequest(request *http.Request) ([]byte, error) {
+    client := &http.Client{}
+
+    response, err := client.Do(request)
+    
+    if err != nil {
+        return nil, err
+    }
+
+    defer response.Body.Close()
+
+    body, err := io.ReadAll(response.Body)
+
+    if err != nil {
+        return nil, err
+    }
+
+    return body, nil
+}
---
+++ utils/util.go
@@ -0,0 +1,134 @@
+package utils
+
+import (
+	"os/exec"
+	"strings"
+    "os"
+    "fmt"
+)
+
+var testCaseList []string
+
+type SessionKey struct {
+    Csrftoken string
+    LeetcodeSession string
+}
+
+func getSessionKey() (*SessionKey, error) {
+
+    session_key := os.Getenv("LEETCODE_SESSION_KEY")
+
+    if session_key == "" {
+        return nil, fmt.Errorf("Couldn't find LEETCODE_SESSION_KEY env")
+    }
+
+    if !strings.Contains(session_key, ";LEETCODE_SESSION=") {
+        return nil, fmt.Errorf("Invalid, did you put LEETCODE_SESSION=?")
+    }
+
+    if !strings.Contains(session_key, "csrftoken=") {
+        return nil, fmt.Errorf("Invalid key, did you put csrftoken=****; ?")
+    }
+
+    idx := strings.Index(session_key, ";")
+
+    if idx == -1 {
+        return nil, fmt.Errorf("Error in parsing session key")
+    }
+
+    csrftoken := session_key[10:idx]
+    leetcode_session := session_key[idx+18 : len(session_key)-1]
+
+    return &SessionKey{ Csrftoken: csrftoken, LeetcodeSession: leetcode_session }, nil
+}
+
+func RenderHTML(html_content string) error {
+
+	cmd := exec.Command("lynx", "-stdin", "-dump")
+	cmd.Stdin = strings.NewReader(html_content)
+
+	file, err := os.Create("problem.txt")
+
+	if err != nil {
+		return err
+	}
+
+	defer file.Close()
+
+	cmd.Stderr = os.Stdout
+	cmd.Stdout = file
+
+	err = cmd.Run()
+
+	if err != nil {
+		return err
+	}
+
+    return nil
+}
+
+func GetTitleSlug(keyword string) string {
+	keyword = strings.ToLower(keyword)
+	list := strings.Split(keyword, " ")
+	return strings.Join(list, "-")
+}
+
+func ParseFileName(fileName string) (questionID string, problemName string, lang string, err error) {
+
+	if !strings.Contains(fileName, ".") {
+		err = fmt.Errorf("No extension found in the given file. Please write the extension, ex. .cpp, .js")
+		return
+	}
+
+	if !strings.Contains(fileName, "_") {
+		err = fmt.Errorf("No question ID found. Write the questionID after problem name with an underscore.\nEx: two-sum_ID.cpp")
+	}
+
+	idx := strings.Index(fileName, ".")
+
+	lang = fileName[idx+1:]
+
+	name := fileName[:idx]
+
+	idx = strings.Index(name, "_")
+
+	problemName = name[:idx]
+
+	questionID = name[idx+1 : strings.Index(fileName, ".")]
+
+	err = nil
+
+	return
+}
+
+func ParseTestCases(content string) (string, error) {
+
+	if content[:2] != "/*" {
+		return "", fmt.Errorf(`Couldn't find /* at the beginning of the file.
+		Make sure to enclose test cases in /* */ comment block at the beginning`)
+	}
+
+	var testCases string
+
+	testCaseList = make([]string,0,3)
+
+	idx := 0
+
+	content = strings.TrimLeft(content[2:],"\t\n ")
+
+	for content[:2] != "*/" {
+		idx = strings.Index(content, ";")
+		if idx == -1 {
+			return "", fmt.Errorf("Couldn't fine ';'. Delimit your test cases with ';'")
+		}
+
+		testCases += content[:idx] + "\n"
+		testCaseList = append(testCaseList, content[:idx] + "\n")
+		content = strings.TrimLeft(content[idx+1:], "\t\n ")
+	}
+
+	testCases = strings.TrimRight(testCases,"\n\t ")
+
+	return testCases, nil
+}
+
