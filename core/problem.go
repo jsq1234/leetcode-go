98faeedcdba48766a25d35c46210cd218ffd95cf
Manan Prakash Singh <mananprakash3078@gmail.com>
2023-07-11 20:28:33 +0530

    added file for fetching problem of the day

--- core/problem.go
+++ core/problem.go
@@ -1,7 +1,8 @@
 package core
 
 import (
-    "github.com/Manan-Prakash-Singh/leetcode-go/utils"
+	"encoding/json"
+	"github.com/Manan-Prakash-Singh/leetcode-go/utils"
 )
 
 const query = `
@@ -14,3 +15,59 @@ query questionOfToday {
         }
     }
 }`
+
+func NewProblemOfTheDayQuery() ([]byte,error){
+    query := GraphqlQuery{
+        Query: query,
+        Variables: nil,
+        OperationName: "questionOfToday",
+    }
+    return json.Marshal(&query)
+}
+
+type ProblemOfTheDayResponse struct {
+	Data struct {
+		ActiveDailyCodingChallengeQuestion struct {
+			Link     string   `json:"link"`
+            Question struct {
+                QuestionID         string `json:"questionId"`
+                QuestionFrontEndID string `json:"questionFrontEndId"`
+                IsPaidyOnly        bool   `json:"isPaidOnly"`
+                Title              string `json:"title"`
+                Difficulty         string `json:"difficulty"`
+                TopicTags          []struct {
+                    Name string `json:"name"`
+                } `json:"topicTags"`
+            } `json:"question"`
+		} `json:"activeDailyCodingChallengeQuestion"`
+	} `json:"data"`
+}
+
+func NewProblemOfTheDayResponse(data []byte) (*ProblemOfTheDayResponse, error){
+    v := ProblemOfTheDayResponse{}
+    err := json.Unmarshal(data,&v)
+    return &v, err
+}
+
+func GetProblemOfTheDay() (*ProblemOfTheDayResponse,error) {
+
+    body, err := NewProblemOfTheDayQuery()
+
+    if err != nil {
+       return nil, err
+    }
+
+    request, err := utils.NewNormalRequest("POST",GRAPHQL_URL,body)
+
+    if err != nil {
+        return nil, err
+    }
+
+    response, err := utils.SendRequest(request)
+
+    if err != nil {
+        return nil, err
+    }
+
+    return NewProblemOfTheDayResponse(response)
+}
