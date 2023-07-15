6b35da46c70071c394f78139ac54a203630ec817
Manan Prakash Singh <mananprakash3078@gmail.com>
2023-07-15 17:02:53 +0530

    formatted

--- cmd/search.go
+++ cmd/search.go
@@ -10,21 +10,21 @@ import (
 
 // searchCmd represents the search command
 var (
-    count int
-    plang string
+	count int
+	plang string
 )
 var searchCmd = &cobra.Command{
 	Use:   "search",
 	Short: "search a problem",
 	Long: `Search a problem from leetcode. Either enclose the problem in double quotes in case of 
 multiple words like "palindrome partionining" or use a dash instead palindrome-partitioning`,
-    Args: cobra.ExactArgs(1),
+	Args: cobra.ExactArgs(1),
 	Run: func(cmd *cobra.Command, args []string) {
-        val := 10
-        if count != 0 {
-            val = 5 
-        }
-        core.SearchProblem(args[0],val)
+		val := 10
+		if count != 0 {
+			val = 5
+		}
+		core.SearchProblem(args[0], val)
 	},
 }
 
@@ -40,5 +40,5 @@ func init() {
 	// Cobra supports local flags which will only run when this command
 	// is called directly, e.g.:
 	// searchCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
-    searchCmd.Flags().IntVarP(&count,"num","n",10,"Number of problems to return on search")
+	searchCmd.Flags().IntVarP(&count, "num", "n", 10, "Number of problems to return on search")
 }
