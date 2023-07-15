feac5ddb60f4ca0452b903acc3df87939ebb1e88
Manan Prakash Singh <mananprakash3078@gmail.com>
2023-07-15 17:05:03 +0530

    formatted

--- core/search.go
+++ core/search.go
@@ -22,51 +22,50 @@ query problemsetQuestionList($categorySlug: String, $limit: Int, $skip: Int, $fi
             }
         }
     }
-}` 
+}`
 
-func newSearchQuery(searchKey string, limit int) ([]byte,error) {
-    query := GraphqlQuery{
-        Query: searchQuery,
-        Variables: map[string]interface{}{
-            "categorySlug" : "",
-            "skip" : 0,
-            "limit" : limit,
-            "filters" : map[string]string{
-                "searchKeywords" : searchKey,
-            },
-        },
-        OperationName: "problemsetQuestionList",
-    }
-    return json.Marshal(&query)
+func newSearchQuery(searchKey string, limit int) ([]byte, error) {
+	query := GraphqlQuery{
+		Query: searchQuery,
+		Variables: map[string]interface{}{
+			"categorySlug": "",
+			"skip":         0,
+			"limit":        limit,
+			"filters": map[string]string{
+				"searchKeywords": searchKey,
+			},
+		},
+		OperationName: "problemsetQuestionList",
+	}
+	return json.Marshal(&query)
 }
 
+func searchProblemRequest(searchKey string, count int) ([]Question, error) {
 
-func searchProblemRequest(searchKey string, count int) ([]Question, error){
-   
-    query, err := newSearchQuery(searchKey,count)
-    if err != nil {
-        return nil, err 
-    }
+	query, err := newSearchQuery(searchKey, count)
+	if err != nil {
+		return nil, err
+	}
 
-    request, err := utils.NewNormalRequest("POST",GRAPHQL_URL,query)
+	request, err := utils.NewNormalRequest("POST", GRAPHQL_URL, query)
 
-    if err != nil {
-        return nil, err 
-    }
+	if err != nil {
+		return nil, err
+	}
 
-    response, err := utils.SendRequest(request)
+	response, err := utils.SendRequest(request)
 
-    if err != nil {
-        return nil, err 
-    }
+	if err != nil {
+		return nil, err
+	}
 
-    data := SearchProblemResponse{}
+	data := SearchProblemResponse{}
 
-    err = json.Unmarshal(response,&data)
+	err = json.Unmarshal(response, &data)
 
-    if err != nil {
-        return nil, err 
-    }
+	if err != nil {
+		return nil, err
+	}
 
-    return data.Data.ProblemsetQuestionList.Questions, nil 
+	return data.Data.ProblemsetQuestionList.Questions, nil
 }
