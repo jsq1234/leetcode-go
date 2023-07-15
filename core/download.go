0f6c41f00f46ddbb29ccbb1067db1e7167d466ee
Manan Prakash Singh <mananprakash3078@gmail.com>
2023-07-15 17:04:40 +0530

    formatted

--- core/download.go
+++ core/download.go
@@ -3,12 +3,12 @@ package core
 import (
 	"encoding/json"
 	"fmt"
-    "io"
-    "os"
 	"github.com/Manan-Prakash-Singh/leetcode-go/utils"
+	"io"
+	"os"
 )
 
-const queryDownload =`
+const queryDownload = `
 query questionContent($titleSlug: String!) {
   question(titleSlug: $titleSlug) {
     content
@@ -25,64 +25,64 @@ query questionContent($titleSlug: String!) {
   }
 }`
 
-func newDownloadProblemQuery(query , problem string) *GraphqlQuery {
-    return &GraphqlQuery{
-        Query: queryDownload,
-        Variables: map[string]interface{}{
-            "titleSlug" : problem,
-        },
-        OperationName: "questionContent",
-    }
+func newDownloadProblemQuery(query, problem string) *GraphqlQuery {
+	return &GraphqlQuery{
+		Query: queryDownload,
+		Variables: map[string]interface{}{
+			"titleSlug": problem,
+		},
+		OperationName: "questionContent",
+	}
 }
 func _downloadProblem(problem, lang string) error {
 
-    problem = utils.GetTitleSlug(problem)  
+	problem = utils.GetTitleSlug(problem)
 
-    queryDownload := newDownloadProblemQuery(query, problem)
+	queryDownload := newDownloadProblemQuery(query, problem)
 
-    query, err := json.Marshal(queryDownload)
+	query, err := json.Marshal(queryDownload)
 
-    if err != nil {
-        return fmt.Errorf("Json Marshalling error %v", err) 
-    }
-    
-    request, err := utils.NewNormalRequest("POST",GRAPHQL_URL,query)
+	if err != nil {
+		return fmt.Errorf("Json Marshalling error %v", err)
+	}
 
-    if err != nil {
-        return fmt.Errorf("Request creation err: %v",err)
-    }
+	request, err := utils.NewNormalRequest("POST", GRAPHQL_URL, query)
 
-    response, err := utils.SendRequest(request)
-    
-    if err != nil {
-        return fmt.Errorf("Response error: %v",err)
-    }
+	if err != nil {
+		return fmt.Errorf("Request creation err: %v", err)
+	}
 
-    data, err := newDownloadProblemResponse(response)
+	response, err := utils.SendRequest(request)
 
-    if err != nil {
-        return fmt.Errorf("Response parsing err: %v", err)
-    }
+	if err != nil {
+		return fmt.Errorf("Response error: %v", err)
+	}
 
-    if data.Data.Question.IsPaidOnly {
-        return fmt.Errorf("The selected problem is premium problem.")    
-    }
+	data, err := newDownloadProblemResponse(response)
 
-    if len(data.Errors) > 0 {
-        return fmt.Errorf("Couldn't find the requested problem. Try doing a search instead.")
-    }
+	if err != nil {
+		return fmt.Errorf("Response parsing err: %v", err)
+	}
 
-    problemHTML := data.Data.Question.Content
+	if data.Data.Question.IsPaidOnly {
+		return fmt.Errorf("The selected problem is premium problem.")
+	}
 
-    if err := utils.RenderHTML(problemHTML); err != nil {
-        return fmt.Errorf("HTML rendering error, did you install lynx? : %v", err)
-    }
- 
-    return createCodeFile(problem,lang,data)
+	if len(data.Errors) > 0 {
+		return fmt.Errorf("Couldn't find the requested problem. Try doing a search instead.")
+	}
+
+	problemHTML := data.Data.Question.Content
+
+	if err := utils.RenderHTML(problemHTML); err != nil {
+		return fmt.Errorf("HTML rendering error, did you install lynx? : %v", err)
+	}
+
+	return createCodeFile(problem, lang, data)
 }
 
 func createCodeFile(problem, lang string, cnt *DownloadProblemResponse) error {
-    
+
 	questionID := cnt.Data.Question.QuestionID
 
 	fileName := problem + "_" + questionID + "." + lang
