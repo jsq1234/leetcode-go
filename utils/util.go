9a5178f9b5c9099872f9b88d64827413dbc53fe0
Manan Prakash Singh <mananprakash3078@gmail.com>
2023-07-15 17:06:46 +0530

    formatted

--- utils/util.go
+++ utils/util.go
@@ -3,85 +3,127 @@ package utils
 import (
 	"bufio"
 	"fmt"
+	"github.com/gookit/color"
+	"github.com/zellyn/kooky"
+	_ "github.com/zellyn/kooky/browser/all"
+	"golang.org/x/net/html"
 	"io"
 	"os"
-    "github.com/zellyn/kooky"
-    _ "github.com/zellyn/kooky/browser/all"
-    "strings"
-    "github.com/gookit/color"
-	"golang.org/x/net/html"
+	"strings"
 )
 
 var TestCaseList []string
 
 type SessionKey struct {
-    Csrftoken string
-    LeetcodeSession string
+	Csrftoken       string
+	LeetcodeSession string
 }
 
 func getCookiesFromBrowser() (*SessionKey, error) {
-     
-    token := SessionKey{}
 
-    cookies := kooky.ReadCookies(
-        kooky.Valid,
-        kooky.DomainHasSuffix(`leetcode.com`),
-        kooky.Name("LEETCODE_SESSION"),
-    )
+	token := SessionKey{}
 
-    if len(cookies) == 0 {
-        return nil, fmt.Errorf("Failed to find LEETCODE_SESSION cookie in any browser. Login into your leetcode account and try again. For now, the program only works if you logic through Chrome/Firefox/Safari browsers.")
-    }
+	cookies := kooky.ReadCookies(
+		kooky.Valid,
+		kooky.DomainHasSuffix(`leetcode.com`),
+		kooky.Name("LEETCODE_SESSION"),
+	)
 
-    token.LeetcodeSession = cookies[0].Value
+	if len(cookies) == 0 {
+		return nil, fmt.Errorf("Failed to find LEETCODE_SESSION cookie in any browser. Login into your leetcode account and try again. For now, the program only works if you logic through Chrome/Firefox/Safari browsers.")
+	}
 
-    cookies = kooky.ReadCookies(
-        kooky.Valid,
-        kooky.DomainHasPrefix("leetcode.com"),
-        kooky.Name("csrftoken"),
-    )
+	token.LeetcodeSession = cookies[0].Value
 
-    if len(cookies) == 0 {
-        return nil, fmt.Errorf("Failed to find LEETCODE_SESSION cookie in any browser. Login into your leetcode account and try again. For now, the program only works if you logic through Chrome/Firefox/Safari browsers.")
-    }
-    
-    token.Csrftoken = cookies[0].Value
+	cookies = kooky.ReadCookies(
+		kooky.Valid,
+		kooky.DomainHasPrefix("leetcode.com"),
+		kooky.Name("csrftoken"),
+	)
 
-    return &token, nil
+	if len(cookies) == 0 {
+		return nil, fmt.Errorf("Failed to find LEETCODE_SESSION cookie in any browser. Login into your leetcode account and try again. For now, the program only works if you logic through Chrome/Firefox/Safari browsers.")
+	}
+
+	token.Csrftoken = cookies[0].Value
+
+	return &token, nil
 }
 
 func getSessionKey() (*SessionKey, error) {
 
-    session_key := os.Getenv("LEETCODE_SESSION_KEY")
+	session_key := os.Getenv("LEETCODE_SESSION_KEY")
 
-    if session_key == "" {
-        return nil, fmt.Errorf("Couldn't find LEETCODE_SESSION_KEY env")
-    }
+	if session_key == "" {
+		return nil, fmt.Errorf("Couldn't find LEETCODE_SESSION_KEY env")
+	}
 
-    if !strings.Contains(session_key, ";LEETCODE_SESSION=") {
-        return nil, fmt.Errorf("Invalid, did you put LEETCODE_SESSION=?")
-    }
+	if !strings.Contains(session_key, ";LEETCODE_SESSION=") {
+		return nil, fmt.Errorf("Invalid, did you put LEETCODE_SESSION=?")
+	}
 
-    if !strings.Contains(session_key, "csrftoken=") {
-        return nil, fmt.Errorf("Invalid key, did you put csrftoken=****; ?")
-    }
+	if !strings.Contains(session_key, "csrftoken=") {
+		return nil, fmt.Errorf("Invalid key, did you put csrftoken=****; ?")
+	}
 
-    idx := strings.Index(session_key, ";")
+	idx := strings.Index(session_key, ";")
 
-    if idx == -1 {
-        return nil, fmt.Errorf("Error in parsing session key")
-    }
+	if idx == -1 {
+		return nil, fmt.Errorf("Error in parsing session key")
+	}
 
-    csrftoken := session_key[10:idx]
-    leetcode_session := session_key[idx+18 : len(session_key)-1]
+	csrftoken := session_key[10:idx]
+	leetcode_session := session_key[idx+18 : len(session_key)-1]
 
-    return &SessionKey{ Csrftoken: csrftoken, LeetcodeSession: leetcode_session }, nil
+	return &SessionKey{Csrftoken: csrftoken, LeetcodeSession: leetcode_session}, nil
 }
 
 func RenderHTML(html_content string) error {
-/*
-	cmd := exec.Command("lynx", "-stdin", "-dump")
-	cmd.Stdin = strings.NewReader(html_content)
+	/*
+		cmd := exec.Command("lynx", "-stdin", "-dump")
+		cmd.Stdin = strings.NewReader(html_content)
+
+		file, err := os.Create("problem.txt")
+
+		if err != nil {
+			return err
+		}
+
+		defer file.Close()
+
+		cmd.Stderr = os.Stdout
+		cmd.Stdout = file
+
+		err = cmd.Run()
+
+		if err != nil {
+			return err
+		}
+
+	    return nil */
+
+	doc, err := html.Parse(strings.NewReader(html_content))
+
+	if err != nil {
+		return err
+	}
+
+	var output string
+	var traverse func(*html.Node)
+
+	traverse = func(n *html.Node) {
+		if n.Type == html.TextNode {
+			if strings.HasPrefix(n.Data, "\n") {
+				n.Data = "\n"
+			}
+			output += n.Data
+		}
+		for c := n.FirstChild; c != nil; c = c.NextSibling {
+			traverse(c)
+		}
+	}
+
+	traverse(doc)
 
 	file, err := os.Create("problem.txt")
 
@@ -91,55 +133,13 @@ func RenderHTML(html_content string) error {
 
 	defer file.Close()
 
-	cmd.Stderr = os.Stdout
-	cmd.Stdout = file
-
-	err = cmd.Run()
+	_, err = io.WriteString(file, output)
 
 	if err != nil {
 		return err
 	}
 
-    return nil */
-    
-    doc, err := html.Parse(strings.NewReader(html_content))
-
-    if err != nil {
-        return err
-    }
-
-    var output string
-    var traverse func(*html.Node) 
-    
-    traverse = func(n* html.Node){ 
-        if n.Type == html.TextNode {
-            if strings.HasPrefix(n.Data,"\n") {
-                n.Data = "\n"
-            }
-            output += n.Data
-        }
-        for c := n.FirstChild; c != nil; c = c.NextSibling {
-           traverse(c)
-        }
-    }
-
-    traverse(doc)
-
-    file, err := os.Create("problem.txt")
-
-    if err != nil {
-        return err
-    }
-
-    defer file.Close()
-
-    _, err = io.WriteString(file,output)
-
-    if err != nil {
-        return err
-    }
-
-    return nil
+	return nil
 }
 
 func GetTitleSlug(keyword string) string {
@@ -150,10 +150,10 @@ func GetTitleSlug(keyword string) string {
 
 func ParseFileName(fileName string) (questionID string, problemName string, lang string, err error) {
 
-    if i := strings.LastIndex(fileName,"/"); i != -1 {
-        fileName = fileName[i+1:]
-    }
-    
+	if i := strings.LastIndex(fileName, "/"); i != -1 {
+		fileName = fileName[i+1:]
+	}
+
 	if !strings.Contains(fileName, ".") {
 		err = fmt.Errorf("No extension found in the given file. Please write the extension, ex. .cpp, .js")
 		return
@@ -189,11 +189,11 @@ func ParseTestCases(content string) (string, error) {
 
 	var testCases string
 
-	TestCaseList = make([]string,0,3)
+	TestCaseList = make([]string, 0, 3)
 
 	idx := 0
 
-	content = strings.TrimLeft(content[2:],"\t\n ")
+	content = strings.TrimLeft(content[2:], "\t\n ")
 
 	for content[:2] != "*/" {
 		idx = strings.Index(content, ";")
@@ -202,31 +202,31 @@ func ParseTestCases(content string) (string, error) {
 		}
 
 		testCases += content[:idx] + "\n"
-		TestCaseList = append(TestCaseList, content[:idx] + "\n")
+		TestCaseList = append(TestCaseList, content[:idx]+"\n")
 		content = strings.TrimLeft(content[idx+1:], "\t\n ")
 	}
 
-	testCases = strings.TrimRight(testCases,"\n\t ")
+	testCases = strings.TrimRight(testCases, "\n\t ")
 
 	return testCases, nil
 }
 
 func Color(difficulty string) string {
-    if difficulty == "Easy" {
-        return color.Green.Sprint(difficulty)
-    }
-    if difficulty == "Medium" {
-        return color.Yellow.Sprintf(difficulty)
-    }
-    if difficulty == "Hard" {
-        return color.Red.Sprintf(difficulty)
-    }
-    return ""
+	if difficulty == "Easy" {
+		return color.Green.Sprint(difficulty)
+	}
+	if difficulty == "Medium" {
+		return color.Yellow.Sprintf(difficulty)
+	}
+	if difficulty == "Hard" {
+		return color.Red.Sprintf(difficulty)
+	}
+	return ""
 }
 
 func UserInput(prompt string) string {
-    fmt.Printf("%v",prompt)
-    scanner := bufio.NewScanner(os.Stdin)
-    scanner.Scan()
-    return scanner.Text()
+	fmt.Printf("%v", prompt)
+	scanner := bufio.NewScanner(os.Stdin)
+	scanner.Scan()
+	return scanner.Text()
 }
