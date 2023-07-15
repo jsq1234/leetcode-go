17344175e9848892cc58cd54d7ac96289a4ee629
Manan Prakash Singh <mananprakash3078@gmail.com>
2023-07-15 17:04:24 +0530

    formatted

--- core/auth.go
+++ core/auth.go
@@ -6,36 +6,36 @@ import (
 	"github.com/Manan-Prakash-Singh/leetcode-go/utils"
 )
 
-type userData struct{
-    UserName string `json:"user_name"`
-    NumSolved int `json:"num_solved"`
-    EasyCount int `json:"ac_easy"`
-    MediumCount int `json:"ac_medium"`
-    HardCount int `json:"ac_hard"`
+type userData struct {
+	UserName    string `json:"user_name"`
+	NumSolved   int    `json:"num_solved"`
+	EasyCount   int    `json:"ac_easy"`
+	MediumCount int    `json:"ac_medium"`
+	HardCount   int    `json:"ac_hard"`
 }
 
-func Authenticate() (*userData,error) {
-    request, err := utils.NewAuthRequest("GET", "https://leetcode.com/api/problems/all", nil)
+func Authenticate() (*userData, error) {
+	request, err := utils.NewAuthRequest("GET", "https://leetcode.com/api/problems/all", nil)
 
-    if err != nil {
-        return nil, err
-    }
-    response, err := utils.SendRequest(request)
+	if err != nil {
+		return nil, err
+	}
+	response, err := utils.SendRequest(request)
 
-    if err != nil {
-        return nil, err
-    }
+	if err != nil {
+		return nil, err
+	}
 
-    data := userData{}
+	data := userData{}
 
-    err = json.Unmarshal(response,&data)
+	err = json.Unmarshal(response, &data)
 
-    if err != nil {
-        return nil, err
-    }
+	if err != nil {
+		return nil, err
+	}
 
-    if data.UserName == "" {
-        return nil, fmt.Errorf("Couldn't authenticate the user. Renew your cookie")
-    }
-    return &data, nil 
+	if data.UserName == "" {
+		return nil, fmt.Errorf("Couldn't authenticate the user. Renew your cookie")
+	}
+	return &data, nil
 }
